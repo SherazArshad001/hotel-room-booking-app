e4979ebecd585eb2810a8dbedd0db06c632ae4f5
Sheraz Baloch <itsbalouch001@gmail.com>
2025-04-07 23:48:09 +0500

    Splash Screen Completed

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -5,6 +5,24 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.brown,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/logo.png",
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Paradise View",
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
